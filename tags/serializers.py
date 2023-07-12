b6c72616587bebfb7e00b3c1ba87e39c941aa262
prasadV05 <135524702+prasadV05@users.noreply.github.com>
2023-07-12 21:47:27 +0530

    serializer file added

--- tags/serializers.py
+++ tags/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from tags.models import Tags
 
-class TagsSerializers(serializers.ModelSerializer):
+# serializer is used to convert orm data into json
+#write operation into database
+class WriteTagsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = ['name']
+
+
+
+
+
+# serializer is used to convert orm data into json
+#read operation into database
+class ReadTagsSerializers(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d/%m/%y")
     class Meta:
         model = Tags
         fields = ['id', 'name','slug','created_at']
